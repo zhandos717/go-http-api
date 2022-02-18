d2ead57fffa0af688a04e3adabf83ec0a2f515fa
Zhandos <zhan96@mail.ru>
2022-02-18 20:14:52 +0600

    0.1

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func HomePage(w http.ResponseWriter, r *http.Request) {
-
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
 }
 
-func main() {
+func (u *User) getAllInfo() string {
+	return fmt.Sprintf("<b>User name is: %s. He is %d and he has money"+
+		" equal: %d</b>", u.Name, u.Age, u.Money)
+}
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	//bob.setNewName("Jonatan")
+	//fmt.Fprintf(w, bob.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
+}
+func AboutPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About")
+}
+func handleRequest() {
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/about/", AboutPage)
 	http.ListenAndServe(":8089", nil)
+}
+func main() {
+	handleRequest()
 }
