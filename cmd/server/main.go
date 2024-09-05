2799860a460d57bb4b45efdf39b17edd5f14cd65
Zhandos <zhandos.zhandarbekov@gmail.com>
2024-09-05 16:10:56 +0500

    Updated index.html with a new line

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/zhandos717/go-http-api/internal/routes"
+	"github.com/zhandos717/go-http-api/pkg/systems"
 )
 
 func main() {
-	// Открытие или создание файла для логов
-	logFile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatalf("Could not open log file : %v", err)
-	}
-	defer logFile.Close()
 
-	// Настройка логгера для записи в файл
-	log.SetOutput(logFile)
+	token := systems.BotToken()
 
-	r := routes.SetupRoutes()
-
-	log.Println("Server started at :8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatalf("Could not start server: %s\n", err.Error())
-	}
+	println(token)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhandos717/go-http-api
 
 go 1.22.5
+
+require github.com/joho/godotenv v1.5.1
